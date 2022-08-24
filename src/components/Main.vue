f4149fb037e3d35a399d2ea2f2b3ef7dd6feb1c0
Valerie K <valerik210695@gmail.com>
2022-08-24 10:54:59 +0300

    project start

---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div className="page">
+    <header className="header">
+      <div className="header__container">
+        <h1 className='header__title'>Pic Finder</h1>
+      </div>
+    </header>
+
+    <main className="gallery">
+      <form className="gallery__form" name="search">
+        <input
+          className="gallery__input"
+          id="name"
+          name="name"
+          type="text"
+          required
+        />
+        <button className="gallery__submit-button" type="submit">Search</button>
+      </form>
+      <button className="gallery__button" type="button">Show random picture</button>
+      <ul className="gallery__card-list"></ul>
+    </main>
+
+    <footer className="footer">
+      <p className="footer__text">&copy; 2022 Pic Finder</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main-component'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <style scoped></style> -->
