28c2f293d9320330435cdc2d1307929fe15c2556
Valerie K <valerik210695@gmail.com>
2022-09-07 17:48:25 +0300

    add pagination

--- src/components/App.vue
+++ src/components/App.vue
@@ -18,33 +18,67 @@ import { getPictures } from "../utils/api";
 
 export default {
   name: 'App',
+
   components: {
     Main,
     Header,
     Footer
   },
+
   data() {
     return {
       cards: [],
       query: '',
       page: 1,
-      selectedCard: null,
+      totalPages: 0,
+      isfetching: false,
+      selectedCard: null
     }
   },
+
   methods: {
     handleSubmit() {
+      this.page = 1;
       getPictures(this.page, this.query)
       .then((cardsData) => {
         this.cards = [...cardsData.results];
-        this.query = '';
+        this.totalPages = cardsData.total_pages;
       })
       .catch((err) => {
         console.log(err);
       })
     },
+
     handleChangeQuery(e) {
       this.query = e.target.value;
+    },
+
+    handleScroll(e) {
+      if (e.target.documentElement.scrollTop + e.target.documentElement.clientHeight >= e.target.documentElement.scrollHeight) {
+        this.isfetching = true;
+        this.page += 1;
+      }
+      if (this.isfetching && this.cards.length < this.totalPages) {
+        getPictures(this.page, this.query)
+        .then((cardsData) => {
+          this.cards = [...this.cards, ...cardsData.results];
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          this.isfetching = false;
+        })
+      }
     }
+  },
+
+  mounted() {
+    document.addEventListener('scroll', this.handleScroll);
+  },
+
+  unmounted() {
+    document.removeEventListener('scroll', this.handleScroll);
   }
 }
 </script>
