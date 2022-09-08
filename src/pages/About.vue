c2dfbf237378440384b845aa6bb6cf939d9e645f
Valerie K <valerik210695@gmail.com>
2022-09-08 19:24:54 +0300

    edit About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,8 +2,8 @@
   <div className="cardInfo">
     <router-link to="/" className="cardInfo__link" @click="onBack">Go back</router-link>
     <figure className="cardInfo__figure">
-      <img className="cardInfo__image" :src="selectedCard.urls.regular" :alt="selectedCard.alt_description" />
-      <figcaption className="cardInfo__text">{{ selectedCard.alt_description }}</figcaption>
+      <img className="cardInfo__image" :src="selectedCard ? selectedCard.urls.regular : ''" :alt="selectedCard ? selectedCard.alt_description : ''" />
+      <figcaption className="cardInfo__text">{{ selectedCard ? selectedCard.alt_description : '' }}</figcaption>
     </figure>
   </div>
 </template>
